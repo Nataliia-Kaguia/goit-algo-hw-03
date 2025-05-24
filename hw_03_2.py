476b5452301f4487c3a15447364052b3f8d7193f
Nataliia-Kaguia <moon.kaguia@gmail.com>
2025-05-24 21:44:57 +0300

    corrected secjnd task

--- hw_03_2.py
+++ hw_03_2.py
@@ -1,48 +1,12 @@
 import random
 
-def generate_lottery_tickets(participant: list)-> dict:
-    all_tickets = set()
-    participant_tickets = {}
+def get_numbers_ticket(min_value, max_value, quantity):
+    # Перевірка валідності параметрів
+    if not (1 <= min_value <= max_value <= 1000) or quantity > (max_value - min_value + 1):
+        return []
 
-    for participant in participants:
-        num_tickets = random.randint(1, 5)
-        tickets = list()
-
-        for _ in range(num_tickets):
-            while True:
-                ticket = tuple(sorted(random.sample(range(1, 50), k=6)))
-                if ticket not in all_tickets:
-                    all_tickets.add(ticket)
-                    tickets.append(ticket)
-                    break
-
-        participant_tickets[participant] = tickets
-
-    return participant_tickets
-
-def pick_random_winner(participant_tickets):
-    all_tickets = []
-    for paticipant, tickets in participant_tickets.items():
-        for ticket in tickets:
-            all_tickets.append((participant, ticket))
-
-    winner = random.choice(all_tickets)
-    winner_name, winning_tickets = winner
-
-    message = (
-        f'Congratulation {winner_name}!\n'
-        f'Your winning ticket {winning_tickets}'
-    )
-
-    return message
-
-participants = ['Alex', 'Viktoriia', 'Jhon', 'Gwen']
-
-participant_tickets = generate_lottery_tickets(participants)
-for participant, tickets in participant_tickets.items():
-    print(f'Participant:{participant}')
-    print('Tickets:')
-    for ticket in tickets:
-        print(f'\t{ticket}')
-
-print(pick_random_winner(participant_tickets))
+    # Генерація унікальних випадкових чисел
+    numbers = random.sample(range(min_value, max_value + 1), quantity)
+    
+    # Повертаємо відсортований список
+    return sorted(numbers)
