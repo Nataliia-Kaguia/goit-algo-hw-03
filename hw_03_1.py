e25320fe80427eb5052a77e2c75f9aac6674709f
Nataliia-Kaguia <moon.kaguia@gmail.com>
2025-05-24 21:40:13 +0300

    first task fixed

--- hw_03_1.py
+++ hw_03_1.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
-def get_days_from_today(date):
-    input_date = datetime.strptime(date, "%Y-%m-%d").date()
-    
-    today = datetime.today().date()
-    
-    delta = today - input_date
-    
-    return delta.days
+def get_days_from_today(date_str):
+    try:
+        # Перетворюємо рядок на об'єкт дати
+        input_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        # Отримуємо поточну дату
+        today = datetime.today().date()
+        # Розраховуємо різницю (в днях)
+        delta = today - input_date
+        # Повертаємо кількість днів
+        return delta.days
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте 'РРРР-ММ-ДД'."
